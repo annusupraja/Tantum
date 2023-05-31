de1199c59cc8ea31f7257c8b3115ae4932b80f0d
annusupraja <123865422+annusupraja@users.noreply.github.com>
2023-05-30 23:58:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let menubar=document.querySelector('#menubar');
+let mynav=document.querySelector('.navbar');
+menubar.onclick=()=>{
+    menubar.classlist.toggle('fa-timer')
+    mynav.classlist.toggle('active')
+}
+
+
+
+
+
